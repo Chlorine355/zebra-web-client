bb491c4251878d78cfcbf6456de1c05171180d9f
Chlorine355 <akimoff.an@gmail.com>
2026-02-26 00:47:11 +0300

    feat: add pagination in users, fix styles

--- src/widgets/UsersTable/UsersTable.tsx
+++ src/widgets/UsersTable/UsersTable.tsx
@@ -1,18 +1,25 @@
-import { Button, Empty, Table } from "antd";
+import { Button, Empty, Pagination, Table } from "antd";
 import { UserType } from "@shared/model/data/types";
 import { COLUMNS } from "./model/data/data";
 import { useEffect, useState } from "react";
 import { loadUsersData } from "./lib/helpers";
 import styles from './UsersTable.module.scss'
+import { PAGE_SIZE } from "@shared/model/data/const";
 
 export const UsersTable = () => {
-    const [data, setData] = useState<UserType[] | null>(null)
+    const [data, setData] = useState<{ users: UserType[], total: number } | null>(null)
+    const [page, setPage] = useState<number>(1);
 
     const load = () => {
-        loadUsersData().then((response) => {
-            setData(response.users);
+        loadUsersData(page - 1).then((response) => {
+            setData(response);
         })
     }
     useEffect(load, [])
-    return <><Button className={styles.button} onClick={load}>Обновить</Button><Table scroll={{ x: 0, y: 400 }} dataSource={data ?? []} columns={COLUMNS} locale={{ emptyText: <Empty description="Нет данных" /> }} /></>
+    return <>
+        <Button onClick={load}>Обновить</Button>
+        <Table scroll={{ x: 0, y: 400 }} pagination={false} dataSource={data?.users ?? []} columns={COLUMNS} locale={{ emptyText: <Empty description="Нет данных" /> }} />
+        <Pagination current={page} onChange={setPage} total={data?.total} pageSize={PAGE_SIZE} />
+
+    </>
 }
