032a91fe549003142ae10c3d45a54b37f2e4a190
aandrey <andrey.akimov@abgroup.tech>
2026-05-20 17:12:56 +0300

    fix: make login persistent

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,15 +1,9 @@
-import { apiService } from "@shared/api/service/apiService";
-import { setCurrentUserEv } from "@shared/auth/model/store.ts/actions";
 import { Heatmap } from "@widgets/Heatmap/HeatMap";
 import { ReportTable } from "@widgets/ReportTable/ReportTable";
 import { UsersTable } from "@widgets/UsersTable/UsersTable";
 import { Tabs, TabsProps } from "antd";
-import { useEffect } from "react";
 
 export const MainPage = () => {
-    useEffect(() => {
-        apiService.users.current().then(({ data }) => { setCurrentUserEv(data) })
-    }, [])
     const items: TabsProps['items'] = [
         {
             key: 'reports',
